db49010d7704b656cb0ccd41e31c36272075e069
Чернышев Антон <black2604@mail.ru>
2022-08-30 10:42:47 +0500

    задача выполнена

--- Задача с собакой/Program.cs
+++ Задача с собакой/Program.cs
@@ -1,23 +1,25 @@
-﻿int Count = 0
-int Distanse = 10000
-int FFS = 1
-int SFS = 2
-int DS = 5
-int Friend = 2
-While (Distanse < 10)
+﻿int Count = 0;
+int Distanse = 10000;
+int FFS = 1;
+int SFS = 2;
+int DS = 5;
+int Friend = 2;
+int Time = 0;
+while (Distanse > 10)
 {
     if (Friend == 1)
     {
-        Time = distanse / (FFS + DS)
-        Friend = 2
+        Time = Distanse / (FFS + DS);
+        Friend = 2;
     }
     if (Friend == 2)
     {
-        Time = distanse / (SFS + DS)
-        Friend = 1
+        Time = Distanse / (SFS + DS);
+        Friend = 1;
     }
-    distanse = distanse - (FFS + SFS) * Time
+    Distanse = Distanse - (FFS + SFS) * Time;
+    Count ++;
 }
 Console.Write("Собака пробежала ");
-Console.Write(max);
+Console.Write(Count);
 Console.WriteLine(" раз");
