00951f7cefda062ceaf80783a4cd05d41eaddb71
Чернышев Антон <black2604@mail.ru>
2022-08-30 10:33:16 +0500

    попытка номер 1

--- Задача с собакой/Program.cs
+++ Задача с собакой/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Count = 0
+int Distanse = 10000
+int FFS = 1
+int SFS = 2
+int DS = 5
+int Friend = 2
+While (Distanse < 10)
+{
+    if (Friend == 1)
+    {
+        Time = distanse / (FFS + DS)
+        Friend = 2
+    }
+    if (Friend == 2)
+    {
+        Time = distanse / (SFS + DS)
+        Friend = 1
+    }
+    distanse = distanse - (FFS + SFS) * Time
+}
+Console.Write("Собака пробежала ");
+Console.Write(max);
+Console.WriteLine(" раз");
